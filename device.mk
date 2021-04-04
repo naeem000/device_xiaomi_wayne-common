729028df8ae7d55b1e736e85e534f0c83e5a3a99
StyloGey <stylogey@gmail.com>
2021-04-04 12:59:40 +0200

    jasmine: commonize torch permission
    Change-Id: I4ca811f710a9ed9882cd5aac12dbda825ec8f39d

--- device.mk
+++ device.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # QTI common
