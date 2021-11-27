0ffc20c4f709f8ea6ef025b97e05988eb154c2d7
StyloGey <stylogey@gmail.com>
2021-11-27 16:21:57 +0100

    jasmine: disable updatable apex

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 IS_BRINGUP := true
 
 # Device Path
